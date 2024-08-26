0e3360400586dfd5cc77512968468aabe9d3b6be
Gabriel Geraldo <gabrielgeraldo165@gmail.com>
2024-08-26 18:20:20 -0300

    Aqui estou iniciando mais um módulo do curso da OneBitCode, um módulo sobre o React, estou no começo do módulo, só estou adicionando 2 pastas e um exercicio.

---
+++ 2 - projetoReact/first-react-project/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Olá, mundo!</h1>
+      <p>Este, é meu primeiro projeto usando React e Vite!</p>
+      <p>Próximos passo:</p>
+      <ul>
+        <li>Praticar com exercícios</li>
+        <li>Aprender a usar dados dinãmicos</li>
+        <li>Criar novos componentes</li>
+        <li>Estilizar os componentes</li>
+        <li>Reutilizar componentes</li>
+      </ul>
+    </div>
+  )
+}
